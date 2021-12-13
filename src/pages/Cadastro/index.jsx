94902899976de65a83e79d45212272ed1061ee88
lucasquearis <lucasquearis@hotmail.com>
2021-12-13 20:41:54 -0300

    inicia componente cadastro, mas nao ira ter tempo para terminar e implementar o back-end

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Footer from "../../components/Footer";
+import covidSafe from "../../img/covidSafe.png";
 
 const Cadastro = () => {
-  return <h1>Eu sou o Cadastro!!</h1>;
+  return (
+    <div>
+      <img alt="selo covid Safe" src={covidSafe} />
+      <h1>Formulário para o cadastro</h1>
+      <h3>Todos os campos são obrigatórios*:</h3>
+      <form>
+        <label>
+          Nome*:
+          <input></input>
+        </label>
+        <label>
+          Sobre você*:
+          <input></input>
+        </label>
+        <label>
+          URL de uma foto*:
+          <input></input>
+        </label>
+        <label>
+          Email*:
+          <input></input>
+        </label>
+        <label>
+          Password*:
+          <input></input>
+        </label>
+      </form>
+      <Footer />
+    </div>
+  );
 };
 
 export default Cadastro;
